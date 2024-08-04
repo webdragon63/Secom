425c0fd5891cd047467d1cdb0ae8b4f3f273fdcd
WebDragon <117004849+webdragon63@users.noreply.github.com>
2024-08-04 13:39:55 -0400

    Add files via upload

---
+++ src/Listener.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+clear
+
+cat<< EOF
+                    _____   ______    _____    ____    __  __ 
+                   / ____| |  ____|  / ____|  / __ \  |  \/  |
+                  | (___   | |__    | |      | |  | | | \  / |
+                   \___ \  |  __|   | |      | |  | | | |\/| |
+                   ____) | | |____  | |____  | |__| | | |  | |
+                  |_____/  |______|  \_____|  \____/  |_|  |_|
+
+ＷＯＲＬＤ＇Ｓ  ＭＯＳＴ  ＳＥＣＵＲＥ  ＣＯＭＭＵＮＩＣＡＴＩＯＮ  ＭＥＤＩＵＭ
+                              ＦＯＲ  ＨＡＣＫＥＲＳ
+________________________________________________________________________________
+
+EOF
+
+read -p " a) TO USE UNENCRYPTED NETWORK COMMUNICATIONS
+ b) TO USE ENCRYPTED NETWORK WITH PASSWORD PROTECTION
+ (Type a or b) >>" ab
+
+         case $ab in
+         a) echo USING UNENCRYPTED NETWORK ... ;;
+         b) echo USING ENCRYPTED NETWORK ... ;
+         bash listener/encrlistener.sh
+         
+esac
+bash listener/unenlistener.sh
+done
